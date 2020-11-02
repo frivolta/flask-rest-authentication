f301d6e64c063aa3d288e260532df4f1be712519
frivolta <rivoltafilippo@gmail.com>
2020-11-02 11:13:33 +0100

    loggin errors

--- auth/__init__.py
+++ auth/__init__.py
@@ -9,12 +9,16 @@ from auth.blueprints.budgets import budgets
 from auth.blueprints.categories import categories
 from auth.blueprints.expenses import expenses
 
+import logging
+
 
 def create_app(environment_name='dev'):
     app = Flask(__name__)
     app.config.from_object(configurations[environment_name])
 
     db.init_app(app)
+    app.logger.addHandler(logging.StreamHandler(sys.stdout))
+    app.logger.setLevel(logging.ERROR)
     migrate.init_app(app, db, render_as_batch=True)
     app.register_blueprint(products, url_prefix="/product")
     app.register_blueprint(users)
